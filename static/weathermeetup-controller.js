934950eefb6010caccb6c875d66c8a620e67290c
Alexander Davis <alexander.davis54@gmail.com>
2017-03-27 22:17:20 -0500

    Update controller.js for getMeetups function

--- static/weathermeetup-controller.js
+++ static/weathermeetup-controller.js
@@ -1,6 +1,24 @@
-class Weather{
+class WeatherController{
   constructor(location){
     this.location=location;
   }
-  getWeather()
+  getWeather() {
+    $.ajax({
+      // url:"http://api.fixer.io/latest",
+      // url:"http://localhost:5000/proxy/mypuppy?i=onions,garlic&q=omelet&p=3",
+      url:`http://localhost:5000/proxy/weather/?city=chicago `,
+      method:"GET"
+    }).done(function(data){
+      console.log(data);
+    });
+  }
+
+  static getMeetups() {
+    $.ajax({
+      url: "http://localhost:8088/proxy/meetupmain?zip=94066&radius=1&category=25&order=members",
+      method: "GET"
+    }).done(function(data) {
+      console.log(data);
+    });
+  }
 }
